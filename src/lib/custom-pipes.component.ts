49533e3abbedb5435aa859486eb5bb38d2bb32dd
Derambakht <derambakht@gmail.com>
2019-12-22 18:47:00 +0330

    first commit

---
+++ src/lib/custom-pipes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-custom-pipes',
+  template: `
+    <p>
+      custom-pipes works!
+    </p>
+  `,
+  styles: []
+})
+export class CustomPipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/lib/custom-pipes.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CustomPipesComponent } from './custom-pipes.component';
+
+
+
+@NgModule({
+  declarations: [CustomPipesComponent],
+  imports: [
+  ],
+  exports: [CustomPipesComponent]
+})
+export class CustomPipesModule { }
